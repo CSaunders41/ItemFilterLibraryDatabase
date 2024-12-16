292865e4acccc77c416d2d9588ff0722ce87ee2d
Detective Squirrel <2137769+DetectiveSquirrel@users.noreply.github.com>
2024-12-16 17:12:12 +1300

    corrected sorting

--- Areas/MyTemplatesArea.cs
+++ Areas/MyTemplatesArea.cs
@@ -1,12 +1,12 @@
-﻿using ImGuiNET;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ImGuiNET;
 using ItemFilterLibraryDatabase.Api;
 using ItemFilterLibraryDatabase.Api.Models;
 using ItemFilterLibraryDatabase.UI;
 using ItemFilterLibraryDatabase.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
 
 namespace ItemFilterLibraryDatabase.Areas;
 
@@ -14,23 +14,20 @@ public class MyTemplatesArea : BaseArea
 {
     private const int ItemsPerPage = 20;
     private const float SearchDelayDuration = 0.5f;
-    private readonly SortState _sortState = new() {Column = SortColumn.Updated, Ascending = false};
+    private readonly SortState _sortState = new() { Column = SortColumn.Updated, Ascending = false };
     private readonly TemplateModal _templateModal;
     private List<Template> _allTemplates = [];
     private int _currentPage = 1;
     private string _errorMessage = string.Empty;
     private List<Template> _filteredTemplates = [];
-    private bool _isRefreshing = false;
-    private float _searchDelay = 0;
+    private bool _isRefreshing;
+    private float _searchDelay;
     private string _searchText = string.Empty;
 
     public MyTemplatesArea(ItemFilterLibraryDatabase plugin, ApiClient apiClient) : base(plugin, apiClient)
     {
         _templateModal = new TemplateModal(plugin, apiClient);
-        if (Plugin.Initialized)
-        {
-            RefreshTemplates();
-        }
+        if (Plugin.Initialized) RefreshTemplates();
     }
 
     public override string Name => "My Templates";
@@ -41,169 +38,134 @@ public class MyTemplatesArea : BaseArea
     }
 
     public override void Draw()
-{
-    ImGui.PushItemWidth(300);
-    if (ImGui.InputText("Search Templates", ref _searchText, 100))
     {
-        _searchDelay = SearchDelayDuration;
-    }
+        ImGui.PushItemWidth(300);
+        if (ImGui.InputText("Search Templates", ref _searchText, 100)) _searchDelay = SearchDelayDuration;
 
-    ImGui.PopItemWidth();
-
-    ImGui.SameLine();
-    if (ImGui.Button("Refresh") && !_isRefreshing)
-    {
-        RefreshTemplates();
-    }
-
-    ImGui.SameLine();
-    if (ImGui.Button("New Template [+]"))
-    {
-        _templateModal.Show(null, TemplateModalMode.Create);
-    }
-
-    ShowError(_errorMessage);
-
-    if (_filteredTemplates.Count > 0)
-    {
-        var totalPages = (_filteredTemplates.Count + ItemsPerPage - 1) / ItemsPerPage;
-
-        if (ImGui.Button("Previous") && _currentPage > 1)
-        {
-            _currentPage--;
-        }
+        ImGui.PopItemWidth();
 
         ImGui.SameLine();
-        ImGui.Text($"Page {_currentPage} of {totalPages}");
-
+        if (ImGui.Button("Refresh") && !_isRefreshing) RefreshTemplates();
         ImGui.SameLine();
-        if (ImGui.Button("Next") && _currentPage < totalPages)
+        if (ImGui.Button("New Template [+]")) _templateModal.Show(null, TemplateModalMode.Create);
+
+        ShowError(_errorMessage);
+
+        if (_filteredTemplates.Count > 0)
         {
-            _currentPage++;
-        }
+            var totalPages = (_filteredTemplates.Count + ItemsPerPage - 1) / ItemsPerPage;
 
-        ImGui.SameLine();
-        var spacing = ImGui.GetStyle().ItemSpacing.X * 4;
-        ImGui.Dummy(new Vector2(spacing, 0));
-        ImGui.SameLine();
-
-        ImGui.Text($"Total templates: {_filteredTemplates.Count}");
-        ImGui.Separator();
-    }
-
-    if (_searchDelay > 0)
-    {
-        _searchDelay -= ImGui.GetIO().DeltaTime;
-        if (_searchDelay <= 0)
-        {
-            FilterTemplates();
-        }
-    }
-
-    if (_filteredTemplates.Count > 0)
-    {
-        DrawTemplatesTable();
-    }
-    else if (_isRefreshing)
-    {
-        ImGui.Text("Loading templates...");
-    }
-    else if (!string.IsNullOrEmpty(_searchText))
-    {
-        ImGui.Text("No templates found matching your search");
-    }
-    else
-    {
-        ImGui.Text($"No templates found for '{ItemFilterLibraryDatabase.Main.Settings.CurrentTemplateType?.Description ?? "selected type"}'");
-    }
-
-    _templateModal.Draw();
-}
-
-private void DrawTemplatesTable()
-{
-    var flags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Sortable | ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable;
-
-    if (ImGui.BeginTable("my_templates", 5, flags))
-    {
-        ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthStretch);
-        ImGui.TableSetupColumn("Public", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
-        ImGui.TableSetupColumn("Updated", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 100);
-        ImGui.TableSetupColumn("Version", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
-        ImGui.TableSetupColumn("Actions", ImGuiTableColumnFlags.NoSort | ImGuiTableColumnFlags.WidthFixed, 240);
-        ImGui.TableHeadersRow();
-
-        var sortSpecs = ImGui.TableGetSortSpecs();
-        if (sortSpecs.SpecsDirty)
-        {
-            var sortSpec = sortSpecs.Specs;
-            _sortState.Column = (SortColumn)sortSpec.ColumnIndex;
-            _sortState.Ascending = sortSpec.SortDirection == ImGuiSortDirection.Ascending;
-            ApplySort();
-            sortSpecs.SpecsDirty = false;
-        }
-
-        var startIndex = (_currentPage - 1) * ItemsPerPage;
-        var pageTemplates = _filteredTemplates.Skip(startIndex).Take(ItemsPerPage).ToList();
-
-        foreach (var template in pageTemplates)
-        {
-            ImGui.PushID($"actions_{template.TemplateId}");
-            ImGui.TableNextRow();
-
-            ImGui.TableNextColumn();
-            ImGui.TextUnformatted(template.Name);
-
-            ImGui.TableNextColumn();
-            if (ImGui.Button(template.IsPublic ? "Public" : "Private", new Vector2(-1, 0)))
-            {
-                ToggleTemplateVisibility(template.TemplateId, !template.IsPublic);
-            }
-
-            ImGui.TableNextColumn();
-            ImGui.TextUnformatted(FormatTimeAgo(template.UpdatedAt));
-
-            ImGui.TableNextColumn();
-            ImGui.TextUnformatted(template.Version.ToString());
-
-            ImGui.TableNextColumn();
-
-            var buttonWidth = (ImGui.GetContentRegionAvail().X - ImGui.GetStyle().ItemSpacing.X * 2) / 3;
-            if (ImGui.Button("Edit", new Vector2(buttonWidth, 0)))
-            {
-                _templateModal.Show(template, TemplateModalMode.Edit);
-            }
+            if (ImGui.Button("Previous") && _currentPage > 1) _currentPage--;
 
             ImGui.SameLine();
-            if (ImGui.Button("Copy", new Vector2(buttonWidth, 0)))
-            {
-                CopyTemplateContent(template.TemplateId);
-            }
+            ImGui.Text($"Page {_currentPage} of {totalPages}");
 
             ImGui.SameLine();
-            if (ImGui.Button("Delete", new Vector2(buttonWidth, 0)))
-            {
-                DeleteTemplate(template.TemplateId);
-            }
+            if (ImGui.Button("Next") && _currentPage < totalPages) _currentPage++;
 
-            ImGui.PopID();
+            ImGui.SameLine();
+            var spacing = ImGui.GetStyle().ItemSpacing.X * 4;
+            ImGui.Dummy(new Vector2(spacing, 0));
+            ImGui.SameLine();
+
+            ImGui.Text($"Total templates: {_filteredTemplates.Count}");
+            ImGui.Separator();
         }
 
-        ImGui.EndTable();
+        if (_searchDelay > 0)
+        {
+            _searchDelay -= ImGui.GetIO().DeltaTime;
+            if (_searchDelay <= 0) FilterTemplates();
+        }
+
+        if (_filteredTemplates.Count > 0)
+            DrawTemplatesTable();
+        else if (_isRefreshing)
+            ImGui.Text("Loading templates...");
+        else if (!string.IsNullOrEmpty(_searchText))
+            ImGui.Text("No templates found matching your search");
+        else
+            ImGui.Text(
+                $"No templates found for '{ItemFilterLibraryDatabase.Main.Settings.CurrentTemplateType?.Description ?? "selected type"}'");
+
+        _templateModal.Draw();
     }
-}
+
+    private void DrawTemplatesTable()
+    {
+        var flags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Sortable |
+                    ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable;
+
+        if (ImGui.BeginTable("my_templates", 5, flags))
+        {
+            ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthStretch);
+            ImGui.TableSetupColumn("Public", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
+            ImGui.TableSetupColumn("Updated", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed,
+                100);
+            ImGui.TableSetupColumn("Version", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
+            ImGui.TableSetupColumn("Actions", ImGuiTableColumnFlags.NoSort | ImGuiTableColumnFlags.WidthFixed, 240);
+            ImGui.TableHeadersRow();
+
+            var sortSpecs = ImGui.TableGetSortSpecs();
+            if (sortSpecs.SpecsDirty)
+            {
+                var sortSpec = sortSpecs.Specs;
+                _sortState.Column = (SortColumn)sortSpec.ColumnIndex;
+                _sortState.Ascending = sortSpec.SortDirection == ImGuiSortDirection.Ascending;
+                ApplySort();
+                sortSpecs.SpecsDirty = false;
+            }
+
+            var startIndex = (_currentPage - 1) * ItemsPerPage;
+            var pageTemplates = _filteredTemplates.Skip(startIndex).Take(ItemsPerPage).ToList();
+
+            foreach (var template in pageTemplates)
+            {
+                ImGui.PushID($"actions_{template.TemplateId}");
+                ImGui.TableNextRow();
+
+                ImGui.TableNextColumn();
+                ImGui.TextUnformatted(template.Name);
+
+                ImGui.TableNextColumn();
+                if (ImGui.Button(template.IsPublic
+                            ? "Public"
+                            : "Private",
+                        new Vector2(-1, 0)))
+                    ToggleTemplateVisibility(template.TemplateId, !template.IsPublic);
+
+                ImGui.TableNextColumn();
+                ImGui.TextUnformatted(FormatTimeAgo(template.UpdatedAt));
+
+                ImGui.TableNextColumn();
+                ImGui.TextUnformatted(template.Version.ToString());
+
+                ImGui.TableNextColumn();
+
+                var buttonWidth = (ImGui.GetContentRegionAvail().X - ImGui.GetStyle().ItemSpacing.X * 2) / 3;
+                if (ImGui.Button("Edit", new Vector2(buttonWidth, 0)))
+                    _templateModal.Show(template, TemplateModalMode.Edit);
+
+                ImGui.SameLine();
+                if (ImGui.Button("Copy", new Vector2(buttonWidth, 0))) CopyTemplateContent(template.TemplateId);
+
+                ImGui.SameLine();
+                if (ImGui.Button("Delete", new Vector2(buttonWidth, 0))) DeleteTemplate(template.TemplateId);
+
+                ImGui.PopID();
+            }
+
+            ImGui.EndTable();
+        }
+    }
 
     private void FilterTemplates()
     {
         if (string.IsNullOrEmpty(_searchText))
-        {
             _filteredTemplates = _allTemplates.ToList();
-        }
         else
-        {
             _filteredTemplates = _allTemplates.Where(template => FuzzyMatcher.FuzzyMatch(_searchText, template.Name))
                 .OrderByDescending(template => FuzzyMatcher.GetMatchScore(_searchText, template.Name)).ToList();
-        }
 
         ApplySort();
         _currentPage = 1;
@@ -212,33 +174,32 @@ private void DrawTemplatesTable()
     private void ApplySort()
     {
         var query = _filteredTemplates.AsQueryable();
-
         query = _sortState.Column switch
         {
             SortColumn.Name => _sortState.Ascending
                 ? query.OrderBy(t => t.Name)
                 : query.OrderByDescending(t => t.Name),
-            SortColumn.Version => _sortState.Ascending
-                ? query.OrderBy(t => t.Version)
-                : query.OrderByDescending(t => t.Version),
-            SortColumn.Updated => _sortState.Ascending
-                ? query.OrderBy(t => t.UpdatedAt)
-                : query.OrderByDescending(t => t.UpdatedAt),
+
             SortColumn.Public => _sortState.Ascending
                 ? query.OrderBy(t => t.IsPublic)
                 : query.OrderByDescending(t => t.IsPublic),
+
+            SortColumn.Updated => _sortState.Ascending
+                ? query.OrderBy(t => long.Parse(t.UpdatedAt))
+                : query.OrderByDescending(t => long.Parse(t.UpdatedAt)),
+
+            SortColumn.Version => _sortState.Ascending
+                ? query.OrderBy(t => t.Version)
+                : query.OrderByDescending(t => t.Version),
+
             _ => query
         };
-
         _filteredTemplates = query.ToList();
     }
 
     private string FormatTimeAgo(string unixTimeStr)
     {
-        if (!long.TryParse(unixTimeStr, out var unixTime))
-        {
-            return "Invalid date";
-        }
+        if (!long.TryParse(unixTimeStr, out var unixTime)) return "Invalid date";
 
         var timestamp = DateTimeOffset.FromUnixTimeSeconds(unixTime);
         var now = DateTimeOffset.UtcNow;
@@ -284,7 +245,9 @@ private void DrawTemplatesTable()
                 return;
             }
 
-            var response = await ApiClient.GetAsync<ApiResponse<List<Template>>>(Routes.Templates.GetMyTemplates(currentType.TypeId));
+            var response =
+                await ApiClient.GetAsync<ApiResponse<List<Template>>>(
+                    Routes.Templates.GetMyTemplates(currentType.TypeId));
 
             _allTemplates = response.Data ?? [];
             FilterTemplates();
@@ -316,12 +279,12 @@ private void DrawTemplatesTable()
                 return;
             }
 
-            var response = await ApiClient.GetAsync<ApiResponse<TemplateDetailed>>(Routes.Templates.GetTemplate(currentType.TypeId, templateId, true));
+            var response =
+                await ApiClient.GetAsync<ApiResponse<TemplateDetailed>>(
+                    Routes.Templates.GetTemplate(currentType.TypeId, templateId, true));
 
             if (response?.Data?.LatestVersion?.Content != null)
-            {
                 ImGui.SetClipboardText(response.Data.LatestVersion.Content.ToString());
-            }
         }
         catch (ApiException ex)
         {
@@ -347,7 +310,8 @@ private void DrawTemplatesTable()
                 return;
             }
 
-            await ApiClient.PatchAsync<ApiResponse<object>>(Routes.Templates.ToggleVisibility(currentType.TypeId, templateId), new {is_public = isPublic});
+            await ApiClient.PatchAsync<ApiResponse<object>>(
+                Routes.Templates.ToggleVisibility(currentType.TypeId, templateId), new { is_public = isPublic });
 
             RefreshTemplates();
         }
@@ -375,7 +339,8 @@ private void DrawTemplatesTable()
                 return;
             }
 
-            await ApiClient.DeleteAsync<ApiResponse<object>>(Routes.Templates.DeleteTemplate(currentType.TypeId, templateId));
+            await ApiClient.DeleteAsync<ApiResponse<object>>(
+                Routes.Templates.DeleteTemplate(currentType.TypeId, templateId));
 
             RefreshTemplates();
         }
@@ -391,10 +356,10 @@ private void DrawTemplatesTable()
 
     private enum SortColumn
     {
-        Name,
-        Version,
-        Updated,
-        Public
+        Name = 0,
+        Public = 1,
+        Updated = 2,
+        Version = 3
     }
 
     private class SortState
--- Areas/PublicTemplatesArea.cs
+++ Areas/PublicTemplatesArea.cs
@@ -1,13 +1,13 @@
-﻿using ImGuiNET;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ImGuiNET;
 using ItemFilterLibraryDatabase.Api;
 using ItemFilterLibraryDatabase.Api.Models;
 using ItemFilterLibraryDatabase.Api.Models.ItemFilterLibraryDatabase.Api.Models;
 using ItemFilterLibraryDatabase.UI;
 using ItemFilterLibraryDatabase.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
 
 namespace ItemFilterLibraryDatabase.Areas;
 
@@ -15,24 +15,21 @@ public class PublicTemplatesArea : BaseArea
 {
     private const float SearchDelayDuration = 0.5f;
     private readonly List<Template> _allTemplates = [];
-    private readonly SortState _sortState = new SortState {Column = SortColumn.Updated, Ascending = false};
+    private readonly SortState _sortState = new() { Column = SortColumn.Updated, Ascending = false };
     private readonly TemplateModal _templateModal;
     private int _currentPage = 1;
     private string _errorMessage = string.Empty;
     private List<Template> _filteredTemplates = [];
     private bool _initialLoadComplete;
     private bool _isLoadingBackground;
-    private PaginationInfo _paginationInfo = new PaginationInfo();
+    private PaginationInfo _paginationInfo = new();
     private float _searchDelay;
     private string _searchText = string.Empty;
 
     public PublicTemplatesArea(ItemFilterLibraryDatabase plugin, ApiClient apiClient) : base(plugin, apiClient)
     {
         _templateModal = new TemplateModal(plugin, apiClient);
-        if (Plugin.Initialized && ApiClient.IsInitialized)
-        {
-            LoadTemplates();
-        }
+        if (Plugin.Initialized && ApiClient.IsInitialized) LoadTemplates();
     }
 
     public override string Name => "Public Templates";
@@ -46,18 +43,12 @@ public class PublicTemplatesArea : BaseArea
         }
 
         ImGui.PushItemWidth(300);
-        if (ImGui.InputText("Search Templates", ref _searchText, 100))
-        {
-            _searchDelay = SearchDelayDuration;
-        }
+        if (ImGui.InputText("Search Templates", ref _searchText, 100)) _searchDelay = SearchDelayDuration;
 
         ImGui.PopItemWidth();
 
         ImGui.SameLine();
-        if (ImGui.Button("Refresh") && !_isLoadingBackground)
-        {
-            LoadTemplates();
-        }
+        if (ImGui.Button("Refresh") && !_isLoadingBackground) LoadTemplates();
 
         if (_isLoadingBackground)
         {
@@ -97,42 +88,34 @@ public class PublicTemplatesArea : BaseArea
         if (_searchDelay > 0)
         {
             _searchDelay -= ImGui.GetIO().DeltaTime;
-            if (_searchDelay <= 0)
-            {
-                FilterTemplates();
-            }
+            if (_searchDelay <= 0) FilterTemplates();
         }
 
         if (_filteredTemplates.Count > 0)
-        {
             DrawTemplatesTable();
-        }
         else if (!_initialLoadComplete || _isLoadingBackground)
-        {
             ImGui.Text("Loading templates...");
-        }
         else if (!string.IsNullOrEmpty(_searchText))
-        {
             ImGui.Text("No templates found matching your search");
-        }
         else
-        {
-            ImGui.Text($"No public templates found for '{ItemFilterLibraryDatabase.Main.Settings.CurrentTemplateType?.Description ?? "selected type"}'");
-        }
+            ImGui.Text(
+                $"No public templates found for '{ItemFilterLibraryDatabase.Main.Settings.CurrentTemplateType?.Description ?? "selected type"}'");
 
         _templateModal.Draw();
     }
 
     private void DrawTemplatesTable()
     {
-        var flags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Sortable | ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable;
+        var flags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Sortable |
+                    ImGuiTableFlags.ScrollY | ImGuiTableFlags.Resizable;
 
         if (ImGui.BeginTable("public_templates", 6, flags))
         {
             ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthStretch);
             ImGui.TableSetupColumn("Author", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 130);
             ImGui.TableSetupColumn("Public", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
-            ImGui.TableSetupColumn("Updated", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 100);
+            ImGui.TableSetupColumn("Updated", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed,
+                100);
             ImGui.TableSetupColumn("Version", ImGuiTableColumnFlags.DefaultSort | ImGuiTableColumnFlags.WidthFixed, 70);
             ImGui.TableSetupColumn("Actions", ImGuiTableColumnFlags.NoSort | ImGuiTableColumnFlags.WidthFixed, 240);
             ImGui.TableHeadersRow();
@@ -171,15 +154,10 @@ public class PublicTemplatesArea : BaseArea
 
                 var buttonWidth = (ImGui.GetContentRegionAvail().X - ImGui.GetStyle().ItemSpacing.X) / 2;
                 if (ImGui.Button("View##", new Vector2(buttonWidth, 0)))
-                {
                     _templateModal.Show(template, TemplateModalMode.View);
-                }
 
                 ImGui.SameLine();
-                if (ImGui.Button("Copy##", new Vector2(buttonWidth, 0)))
-                {
-                    CopyTemplateContent(template.TemplateId);
-                }
+                if (ImGui.Button("Copy##", new Vector2(buttonWidth, 0))) CopyTemplateContent(template.TemplateId);
 
                 ImGui.PopID();
             }
@@ -191,14 +169,10 @@ public class PublicTemplatesArea : BaseArea
     private void FilterTemplates()
     {
         if (string.IsNullOrEmpty(_searchText))
-        {
             _filteredTemplates = _allTemplates.ToList();
-        }
         else
-        {
             _filteredTemplates = _allTemplates.Where(template => FuzzyMatcher.FuzzyMatch(_searchText, template.Name))
                 .OrderByDescending(template => FuzzyMatcher.GetMatchScore(_searchText, template.Name)).ToList();
-        }
 
         ApplySort();
     }
@@ -206,25 +180,36 @@ public class PublicTemplatesArea : BaseArea
     private void ApplySort()
     {
         var query = _filteredTemplates.AsQueryable();
-
         query = _sortState.Column switch
         {
-            SortColumn.Name => _sortState.Ascending ? query.OrderBy(t => t.Name) : query.OrderByDescending(t => t.Name),
-            SortColumn.Author => _sortState.Ascending ? query.OrderBy(t => t.CreatorName) : query.OrderByDescending(t => t.CreatorName),
-            SortColumn.Updated => _sortState.Ascending ? query.OrderBy(t => t.UpdatedAt) : query.OrderByDescending(t => t.UpdatedAt),
-            SortColumn.Version => _sortState.Ascending ? query.OrderBy(t => t.Version) : query.OrderByDescending(t => t.Version),
+            SortColumn.Name => _sortState.Ascending
+                ? query.OrderBy(t => t.Name)
+                : query.OrderByDescending(t => t.Name),
+
+            SortColumn.Author => _sortState.Ascending
+                ? query.OrderBy(t => t.CreatorName)
+                : query.OrderByDescending(t => t.CreatorName),
+
+            SortColumn.Public => _sortState.Ascending
+                ? query.OrderBy(t => t.IsPublic)
+                : query.OrderByDescending(t => t.IsPublic),
+
+            SortColumn.Updated => _sortState.Ascending
+                ? query.OrderBy(t => long.Parse(t.UpdatedAt))
+                : query.OrderByDescending(t => long.Parse(t.UpdatedAt)),
+
+            SortColumn.Version => _sortState.Ascending
+                ? query.OrderBy(t => t.Version)
+                : query.OrderByDescending(t => t.Version),
+
             _ => query
         };
-
         _filteredTemplates = query.ToList();
     }
 
     private string FormatTimeAgo(string unixTimeStr)
     {
-        if (!long.TryParse(unixTimeStr, out var unixTime))
-        {
-            return "Invalid date";
-        }
+        if (!long.TryParse(unixTimeStr, out var unixTime)) return "Invalid date";
 
         var timestamp = DateTimeOffset.FromUnixTimeSeconds(unixTime);
         var now = DateTimeOffset.UtcNow;
@@ -250,10 +235,7 @@ public class PublicTemplatesArea : BaseArea
 
     public override void RefreshData()
     {
-        if (ApiClient.IsInitialized)
-        {
-            LoadTemplates();
-        }
+        if (ApiClient.IsInitialized) LoadTemplates();
     }
 
     private async void LoadTemplates(bool resetCache = true)
@@ -278,7 +260,9 @@ public class PublicTemplatesArea : BaseArea
                 _currentPage = 1;
             }
 
-            var response = await ApiClient.GetAsync<PublicTemplateListResponse>(Routes.Templates.GetAllTemplates(currentType.TypeId, _currentPage));
+            var response =
+                await ApiClient.GetAsync<PublicTemplateListResponse>(
+                    Routes.Templates.GetAllTemplates(currentType.TypeId, _currentPage));
 
             if (response?.Templates != null)
             {
@@ -318,12 +302,12 @@ public class PublicTemplatesArea : BaseArea
                 return;
             }
 
-            var response = await ApiClient.GetAsync<ApiResponse<TemplateDetailed>>(Routes.Templates.GetTemplate(currentType.TypeId, templateId, true));
+            var response =
+                await ApiClient.GetAsync<ApiResponse<TemplateDetailed>>(
+                    Routes.Templates.GetTemplate(currentType.TypeId, templateId, true));
 
             if (response?.Data?.LatestVersion?.Content != null)
-            {
                 ImGui.SetClipboardText(response.Data.LatestVersion.Content.ToString());
-            }
         }
         catch (ApiException ex)
         {
@@ -342,10 +326,11 @@ public class PublicTemplatesArea : BaseArea
 
     private enum SortColumn
     {
-        Name,
-        Author,
-        Updated,
-        Version
+        Name = 0,
+        Author = 1,
+        Public = 2,
+        Updated = 3,
+        Version = 4
     }
 
     private class SortState
